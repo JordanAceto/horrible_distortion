12a46ccd3e2971015fe49ce95bd679bb15796520
jordan aceto <jordanaceto@gmail.com>
2020-08-22 17:08:43 -0400

    test the ADC

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -67,16 +67,70 @@
 int main(void)
 {
 
+    uint32_t delay_time_mSec = 1000;
+
+    uint32_t bit_resolution_reading;
+    uint32_t sample_rate_reading;
+
+    // blink the LED on PA8
+    GPIOA->BSRR = GPIO_BSRR_BS_8;
+
+    SysTick_Delay_mSec(delay_time_mSec);
+
+    GPIOA->BSRR = GPIO_BSRR_BR_8;
+    
+    SysTick_Delay_mSec(delay_time_mSec);
+
     while(1)
     {
-        // blink the LED on PA8
-        GPIOA->BSRR = GPIO_BSRR_BS_8;
 
-        SysTick_Delay_mSec(100);
+        // start a conversion
+        ADC1->CR |= ADC_CR_ADSTART;
 
-        GPIOA->BSRR = GPIO_BSRR_BR_8;
-        
-        SysTick_Delay_mSec(100);
+        while (!(ADC1->ISR & ADC_ISR_EOC))
+        {
+            // wait for conversion to complete
+        }
+
+        bit_resolution_reading = ADC1->DR;
+
+        // start a conversion
+        ADC1->CR |= ADC_CR_ADSTART;
+
+        while (!(ADC1->ISR & ADC_ISR_EOC))
+        {
+            // wait for conversion to complete
+        }
+
+        sample_rate_reading = ADC1->DR;
+
+        while (!(ADC1->ISR & ADC_ISR_EOS))
+        {
+            // wait for the end of the regular sequence
+        }
+
+        // clear the EOS flag
+        ADC1->ISR |= ADC_ISR_EOS;
+
+        // turn the green LED on when the bits pot is turned up more than 1/2 way
+        if (bit_resolution_reading < 2000)
+        {
+            GPIOA->BSRR = GPIO_BSRR_BS_8;
+        }
+        else
+        {
+            GPIOA->BSRR = GPIO_BSRR_BR_8;
+        }
+
+        // turn the red LED on when the sample rate pot is turned up more than 1/2 way
+        if (sample_rate_reading < 2000)
+        {
+            GPIOA->BSRR = GPIO_BSRR_BS_9;
+        }
+        else
+        {
+            GPIOA->BSRR = GPIO_BSRR_BR_9;
+        }  
     }
 }
 
